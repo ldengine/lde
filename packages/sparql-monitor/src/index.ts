f89bba101ff22fdc203133ebefd24fdbc7b27365
David de Boer <david@ddeboer.nl>
2026-01-23 15:25:19 +0100

    feat(sparql-monitor): export SparqlMonitor class and options type
    * Add SparqlMonitor and SparqlMonitorOptions to public exports
    * Allow consumers to use SparqlMonitor directly for custom integrations

--- packages/sparql-monitor/src/index.ts
+++ packages/sparql-monitor/src/index.ts
@@ -1,4 +1,5 @@
 export type { MonitorConfig, Observation, ObservationStore } from './types.js';
+export { SparqlMonitor, type SparqlMonitorOptions } from './monitor.js';
 export { PostgresObservationStore } from './store.js';
 export { MonitorService, type MonitorServiceOptions } from './service.js';
 export { defineConfig, type SparqlMonitorConfig } from './config.js';
